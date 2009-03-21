c4e7fdac34e03c730a94aabee865e01936019d21
eklinger <eklinger@gmail.com>
2009-03-21 05:11:04 +0000

    Update jni functions

--- bindings/java/pHash-jni.cpp
+++ bindings/java/pHash-jni.cpp
@@ -4,11 +4,43 @@
 #include "pHash.h"
 #include "audiophash.h"
 
-JNIEXPORT jint JNICALL Java_pHash_distance
+JNIEXPORT jint JNICALL Java_pHash_imageDistance
   (JNIEnv *e, jclass cl, jlong hash1, jlong hash2)
 {
     
-	return ph_hamming_distance(hash1, hash2);
+	return ph_hamming_distance((ulong64)hash1, (ulong64)hash2);
+	
+}
+JNIEXPORT jdouble JNICALL Java_pHash_audioDistance
+  (JNIEnv *e, jclass cl, jintArray hash1, jintArray hash2)
+{
+
+	const float threshold = 0.30;
+    	const int block_size = 256;
+    	int Nc;
+	double maxC = 0.0, *pC; 
+	jint hash1_len, hash2_len;
+	hash1_len = e->GetArrayLength(hash1);
+	hash2_len = e->GetArrayLength(hash2);
+	if(hash1_len <= 0 || hash2_len <= 0)
+		return -1;
+	jint *hash1_n, *hash2_n;
+	hash1_n = (jint *)malloc(sizeof(jint)*hash1_len);
+	hash2_n = (jint *)malloc(sizeof(jint)*hash2_len);
+	
+	e->GetIntArrayRegion(hash1, 0, hash1_len, hash1_n);
+	e->GetIntArrayRegion(hash2, 0, hash2_len, hash2_n);
+	pC = ph_audio_distance_ber((uint32_t*)hash1_n, hash1_len, (uint32_t*)hash2_n, hash2_len, threshold, block_size, Nc);
+	free(hash1_n);
+	free(hash2_n);
+	maxC = 0.0;
+        for (int j=0;j<Nc;j++){
+            if (pC[j] > maxC){
+                maxC = pC[j];
+            }
+	}
+	delete[] pC;
+	return maxC;
 	
 }
 
@@ -57,7 +89,7 @@ JNIEXPORT jintArray JNICALL Java_pHash_audioHash
     free(buf);
     ret = e->NewIntArray(nbframes);
 
-    e->SetIntArrayRegion(ret, 0, nbframes, hash);
+    e->SetIntArrayRegion(ret, 0, nbframes, (jint *)hash);
 
     free(hash);
 
--- bindings/java/pHash.java
+++ bindings/java/pHash.java
@@ -5,8 +5,26 @@ public class pHash
 	native static long videoHash(String file);
 	native static int[]  audioHash(String file);
 	native static long imageHash(String file);
-	native static int distance(long hash1, long hash2);
+	native static int imageDistance(long hash1, long hash2);
+	native static int audioDistance(int[] hash1, int[] hash2);
+	static int videoDistance(long hash1, long hash2)
+	{
+		return imageDistance(hash1, hash2);
+	}
 	static {
 		System.loadLibrary("pHash-jni");
 	}
+
+
+	public static void main(String args[])
+	{
+		long hash = imageHash(args[0]);
+		long hash2 = imageHash(args[1]);
+		System.out.println(hash);
+		System.out.println(hash2);
+		System.out.println(imageDistance(hash,hash2));
+
+	}
+
+
 }
