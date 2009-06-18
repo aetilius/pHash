3933a21a70af70a41c11f798b61cd4739086f500
dstarkweather <dstarkweather@a949317a-a657-0410-a7e4-a2bba0c30cb0>
2009-06-18 18:05:12 +0000

    mvp funcs

--- trunk/build_mvptree.cpp
+++ trunk/build_mvptree.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
-#include <pHash.h>
+#include <dirent.h>
+#include "pHash.h"
+
+
 
 int main(int argc, char **argv){
  
@@ -14,8 +17,7 @@ int main(int argc, char **argv){
 
 
     ulong64 tmphash = 0;
-
-    /*
+    
     int nbfiles = 0;
     printf("dir name: %s\n", dir_name);
     char **files = readfilenames(dir_name,nbfiles);
@@ -56,61 +58,13 @@ int main(int argc, char **argv){
 	hashlist[count]->hash_length = 1;
          count++;
     }
-    */
-    
-    /*
-    printf("save files to mvp file format ...\n");
+
+ 
+
     if (ph_save_mvptree(&mvpfile, hashlist, count) < 0){
 	printf("unable to save %s\n", filename);
 	exit(1);
     }
-    */
-
-    /*
-    printf("add files to file %s\n", filename);
-    int n = ph_add_mvptree(&mvpfile, hashlist, count);
-    printf("number saved %d out of %d\n", n,count);
-    if (n <= 0){
-	printf("unable to add points to %s\n", filename);
-    }
-    */
-
-    char c = 'y';
-    char queryfile[256];
-    float radius = 21.0;
-    const int knearest = 20;
-    DP *results[knearest];
-    int nbfound;
-
-    DP *query = ph_malloc_datapoint(mvpfile.hash_type, mvpfile.pathlength);
-    do {
-	nbfound = 0;
-	printf("enter file:");
-	fgets(queryfile, 100, stdin);
-	queryfile[strlen(queryfile)-1] = '\0';
-	if (ph_dct_imagehash(queryfile, tmphash) < 0)
-	    continue;
-	query->id = strdup(queryfile);
-	query->hash = malloc(Uint64Array);
-	ulong64 *ptrHash = (ulong64*)query->hash;
-	ptrHash[0] = tmphash; 
-
-	printf("search for: %s\n", queryfile);
-	int res;
-	if ((res = ph_query_mvptree(&mvpfile, query, knearest, radius, results, &nbfound)) != 0){
-	    printf("could not complete query, ret result %d\n",res);
-	    continue;
-	}
-	printf(" %d files found\n", nbfound);
-	for (int i=0;i<nbfound;i++){
-	    ulong64 *hash = (ulong64*)results[i]->hash;
-	    printf(" %d %s %llx\n", i, results[i]->id, *hash);
-	}
-	printf("again? y/n:");
-	c = fgetc(stdin);
-	getchar();
-    } while (c != 'n');
-
 
     return 0;
 }
