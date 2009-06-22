f85b819987e1b98fa532f7e16146f9987b6305e7
eklinger <eklinger@a949317a-a657-0410-a7e4-a2bba0c30cb0>
2009-06-22 21:35:28 +0000

    Add configure script

--- trunk/bindings/java/pHash.java
+++ trunk/bindings/java/pHash.java
@@ -2,33 +2,80 @@ import java.util.*;
 
 public class pHash
 {
-	native static long videoHash(String file);
-	native static int[]  audioHash(String file);
-	native static long imageHash(String file);
-	native static int imageDistance(long hash1, long hash2);
-	native static double audioDistance(int[] hash1, int[] hash2);
-	static int videoDistance(long hash1, long hash2)
+	abstract class Hash
 	{
-		return imageDistance(hash1, hash2);
+		String name;
 	}
+	class AudioHash extends Hash 
+	{
+		int[] hash;
+	}
+	class TextHash extends Hash 
+	{
+		String[] hash;
+	}
+	class VideoHash extends Hash 
+	{
+		long[] hash;
+	}
+	class ImageHash extends Hash 
+	{
+		long hash;
+	}
+	class MVPTree
+	{
+		String mvpFile;
+
+		private MVPTree() {}
+		public native boolean create(String filename, Hash[] hashes);
+		public native Hash[] query(String file, float radius, int maxResults);
+		public native boolean add(Hash[] hashes);
+	}
+
+	native static VideoHash videoHash(String file);
+	native static AudioHash audioHash(String file);
+	native static ImageHash imageHash(String file);
+	native static TextHash textHash(String file);
+	native static int imageDistance(ImageHash hash1, ImageHash hash2);
+	native static double audioDistance(AudioHash hash1, AudioHash hash2);
+	native static int videoDistance(VideoHash hash1, VideoHash hash2);
+	native static int textDistance(TextHash txtHash1, TextHash txtHash2);
+	native static void pHashInit();
 	static {
 		System.loadLibrary("pHash-jni");
+		pHashInit();
 	}
 
 
 	public static void main(String args[])
 	{
-		//long hash = imageHash(args[0]);
-		//long hash2 = imageHash(args[1]);
-		//System.out.println(imageDistance(hash,hash2));
-		int[] audioHash1 = audioHash(args[0]);
-		
-		int[] audioHash2 = audioHash(args[1]);
-		System.out.println("Hashes match: " + Arrays.equals(audioHash1,audioHash2));
-System.out.println("cs = " + audioDistance(audioHash1,audioHash2));
-//		long vHash = videoHash(args[4]);
-//		long vHash2 = videoHash(args[5]);
-//		System.out.println(videoDistance(vHash,vHash2));
+			int i = 0;
+			if(args[i].equals("-a"))
+			{
+				AudioHash audioHash1 = audioHash(args[1]);
+				AudioHash audioHash2 = audioHash(args[2]);
+				System.out.println("cs = " + audioDistance(audioHash1,audioHash2));
+			}
+			else if(args[i].equals("-i"))
+			{
+				ImageHash imHash = imageHash(args[1]);
+				ImageHash imHash2 = imageHash(args[2]);
+				System.out.println(imageDistance(imHash,imHash2));
+			}
+			else if(args[i].equals("-v"))
+			{
+				VideoHash vHash = videoHash(args[1]);
+				VideoHash vHash2 = videoHash(args[2]);
+				System.out.println(videoDistance(vHash,vHash2));
+			}
+			else if(args[i].equals("-t"))
+			{
+				TextHash txtHash = textHash(args[1]);
+				TextHash txtHash2 = textHash(args[2]);
+                                System.out.println(textDistance(txtHash,txtHash2));
+			}
+
+
 
 	}
 
