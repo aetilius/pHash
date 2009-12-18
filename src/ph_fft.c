02c10fb56e603ff4fae9b9dcd1f9f703e5ad1dae
eklinger <eklinger@gmail.com>
2009-12-18 04:25:44 +0000

    Remove FFTW dependency, cleanups in fft code, only install audiophash.h when audio hash is built

--- src/ph_fft.c
+++ src/ph_fft.c
@@ -20,12 +20,10 @@ void fft_calc(int N,double *x,complex double *X,complex double *P,int step,compl
     int k;
     for (k=0;k<N/2;k++){
 	P[k] = P[k]*twids[k*step];
-        nbmults += 2;
     }
     for (k=0;k<N/2;k++){
 	X[k] =     S[k] + P[k];
 	X[k+N/2] = S[k] - P[k];
-	nbadds += 2;
     }
     
 
@@ -42,8 +40,6 @@ int fft(double *x, int N, complex double *X){
     for (k=0;k<N/2;k++){
 	twiddle_factors[k] = polar_to_complex(1.0, 2.0*PI*k/N);
     }
-    nbadds = 0;
-    nbmults = 0;
     fft_calc(N, x, X, Xt, 1, twiddle_factors);
 
     free(twiddle_factors);
