6e9bff48480a4de5553e067b1f795fea880fb511
D G Starkweather <starkdg@comcast.net>
2009-12-21 02:12:35 +0000

    fix getnumbervideoframes func to work faster

--- src/ph_fft.c
+++ src/ph_fft.c
@@ -1,3 +1,29 @@
+/*
+
+    pHash, the open source perceptual hash library
+    Copyright (C) 2009 Aetilius, Inc.
+    All rights reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Evan Klinger - eklinger@phash.org
+    David Starkweather - dstarkweather@phash.org
+
+*/
+
+
+
 #include "ph_fft.h"
 
 complex double polar_to_complex(double r, double theta){
